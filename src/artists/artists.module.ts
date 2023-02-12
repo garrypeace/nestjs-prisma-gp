61b830d53049489c3c3c787164f7115b9ba39af6
Garry Peace <garrypeace@gmail.com>
2023-02-12 11:22:55 +0000

    First use of PrismaService, demonstrating succesful flow from browser -> server -> prisma -> database, and back.

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ArtistsService } from './artists.service';
 import { ArtistsController } from './artists.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ArtistsController],
-  providers: [ArtistsService]
+  providers: [ArtistsService],
 })
 export class ArtistsModule {}
--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ArtistsService {
+  constructor(private prismaService: PrismaService) {}
+
   create(createArtistDto: CreateArtistDto) {
     return 'This action adds a new artist';
   }
 
   findAll() {
-    return `This action returns all artists`;
+    return this.prismaService.artist.findMany();
   }
 
   findOne(id: number) {
