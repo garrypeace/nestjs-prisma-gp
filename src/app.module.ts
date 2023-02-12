09b950eff6e1d7b0cb9acd6bdce58dc386c1e6d7
Garry Peace <garrypeace@gmail.com>
2023-02-12 11:04:56 +0000

    Created Prisma module and service which extends the generated PrismaClient

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArtistsModule } from './artists/artists.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ArtistsModule],
+  imports: [ArtistsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
