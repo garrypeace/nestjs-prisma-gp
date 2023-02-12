8ab6281f02842f97bb360ae8160d2643cca3e387
Garry Peace <garrypeace@gmail.com>
2023-02-12 13:28:22 +0000

    Add Swagger decorators to all endpoints.

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -6,36 +6,44 @@ import {
   Patch,
   Param,
   Delete,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { ArtistsService } from './artists.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { ArtistEntity } from './entities/artist.entity';
 
 @Controller('artists')
 export class ArtistsController {
   constructor(private readonly artistsService: ArtistsService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: ArtistEntity })
   create(@Body() createArtistDto: CreateArtistDto) {
     return this.artistsService.create(createArtistDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: ArtistEntity, isArray: true })
   findAll() {
     return this.artistsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  @ApiOkResponse({ type: ArtistEntity })
+  findOne(@Param('id', ParseIntPipe) id: string) {
     return this.artistsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({ type: ArtistEntity })
   update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
     return this.artistsService.update(id, updateArtistDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: ArtistEntity })
   remove(@Param('id') id: string) {
     return this.artistsService.remove(id);
   }
--- src/artists/entities/artist.entity.ts
+++ src/artists/entities/artist.entity.ts
@@ -1,1 +1,10 @@
-export class Artist {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Artist } from '@prisma/client';
+
+export class ArtistEntity implements Artist {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  name: string;
+}
